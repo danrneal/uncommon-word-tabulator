fef94d3de7c19fb03f3bc55ff5172ef86fa54c7e
Daniel R. Neal <dan.r.neal@gmail.com>
2019-11-01 16:35:38 -0700

    First FT

---
+++ functional_tests.py/test_command_output_correct.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+import unittest
+
+
+class OutputTest(unittest.TestCase):
+
+    def test_cli_takes_txt_file_and_delivers_correct_tabulation(self):
+        # Abe wants to know how many times a the word 'Alice' appears in
+        # alice.txt. He types the appropriate command and discovers the
+        # answer in the output.
+        output = subprocess.check_output(
+            'python uncommonwords.py common.txt alice.txt',
+            stderr=subprocess.STDOUT,
+            shell=True
+        )
+        self.assertIn('Alice:    10,203', output)

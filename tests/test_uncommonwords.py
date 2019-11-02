8d2fcdbaf2abab06d2fbe96d833bcc1f56c661cb
Daniel R. Neal <dan.r.neal@gmail.com>
2019-11-01 19:59:32 -0700

    sort and format words, unit test passes

--- tests/test_uncommonwords.py
+++ tests/test_uncommonwords.py
@@ -1,6 +1,9 @@
 import unittest
 from unittest.mock import mock_open, patch
-from uncommonwords import get_words_from_file, count_uncommon_words
+from uncommonwords import (
+    get_words_from_file, count_uncommon_words, sort_and_format_output
+)
+
 
 @patch(
     'builtins.open',
@@ -26,3 +29,18 @@ class CountUncommonWordsTest(unittest.TestCase):
             "are": 2,
             "not": 1
         })
+
+
+class SortAndFormatOutputTest(unittest.TestCase):
+
+    def test_words_are_sorted_and_formatted(self):
+        uncommon_words = {
+            'some': 1,
+            'very': 5,
+            'uncommon': 2,
+            'words': 3
+        }
+        output = sort_and_format_output(uncommon_words)
+        self.assertEqual(
+            output,
+            "Very:      5\nWords:     3\nUncommon:  2\nSome:      1")
--- uncommonwords.py
+++ uncommonwords.py
@@ -5,6 +5,7 @@ def get_words_from_file(filename):
     with open(filename) as f:
         return re.findall(r'\w+', f.read().lower())
 
+
 def count_uncommon_words(common_words, text):
     uncommon_words = {}
     for word in text:
@@ -13,3 +14,21 @@ def count_uncommon_words(common_words, text):
         elif word not in common_words:
             uncommon_words[word] += 1
     return uncommon_words
+
+
+def sort_and_format_output(uncommon_words):
+    line_length = (((
+        len(max(uncommon_words, key=len)) + 2 +
+        len(str(max(uncommon_words.values())))
+    ) // 4) + 1) * 4
+    sorted_uncommon_words = sorted(
+        uncommon_words.items(),
+        key=lambda kv: kv[1],
+        reverse=True
+    )
+    output = ''
+    for word, tab in sorted_uncommon_words:
+        num_spaces = line_length - len(word) - len(str(tab)) - 1
+        output += f"{word.capitalize()}:{' ' * num_spaces}{tab}\n"
+    output = output[:-1]
+    return output
